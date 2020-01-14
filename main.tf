6be8b719bb8cfa37777399632afed07b8f4c8212
lsimeonov <lsimeonov@despark.com>
2020-01-14 10:47:34 +0200

    Syntax fix for depends_on

--- main.tf
+++ main.tf
@@ -23,7 +23,7 @@ resource "aws_db_event_subscription" "default" {
     "recovery",
   ]
 
-  depends_on = ["aws_sns_topic_policy.default"]
+  depends_on = [aws_sns_topic_policy.default]
 }
 
 resource "aws_sns_topic_policy" "default" {
